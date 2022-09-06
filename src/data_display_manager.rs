f11c592cbf5bac3a6f09eaa5897c01115c386389
sl-cu <sl@cyberunit.tech>
2022-09-06 21:20:14 +0200

    Structurize word pairs in tuples

--- src/data_display_manager.rs
+++ src/data_display_manager.rs
@@ -26,10 +26,20 @@ pub mod data_display_manager {
         shuffled_pairs
     }
 
-    // TODO: always shuffle data first
+    fn structurize_word_pairs_in_tuples(word_pairs: WordPairs) -> Vec<(String, String)> {
+        let vec_of_tuples = word_pairs
+            .iter()
+            .map(|pair| (pair.eng.clone(), pair.ru.clone()));
+
+        vec_of_tuples.collect()
+    }
+
     pub fn show_all_pairs(file_data: &FileData) {
         if let Some(word_pairs) = file_data {
-            let shuffled = shuffle_pairs(word_pairs);
+            let shuffled_pairs = shuffle_pairs(word_pairs);
+            let word_pair_tuples = structurize_word_pairs_in_tuples(shuffled_pairs);
+
+            println!("{:#?}", word_pair_tuples);
 
             return;
         }
@@ -37,9 +47,11 @@ pub mod data_display_manager {
         println!("No data to show");
     }
 
-    // TODO: always shuffle data first
     pub fn exam(file_data: &FileData) {
         if let Some(word_pairs) = file_data {
+            let shuffled_pairs = shuffle_pairs(word_pairs);
+            let word_pair_tuples = structurize_word_pairs_in_tuples(shuffled_pairs);
+
             return;
         }
 
